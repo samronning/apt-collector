f3b261d3b03936775071ffc0906eb8b8a392a462
Sam Ronning <sronning@umich.edu>
2022-02-03 01:05:34 -0500

    allow state search

--- routes/cities.py
+++ routes/cities.py
@@ -2,9 +2,9 @@ from .postgres import connect_postgres, close_postgres
 from flask import request
 from flask_restful import Resource
 
-def top_5_cities_by_search(search="jersey"):
+def top_5_cities_by_search(search):
   connection, cursor = connect_postgres()
-  cursor.execute("SELECT * from us_cities where city iLIKE %(search)s or zips iLIKE %(search)s", {"search": f"{search}%"})
+  cursor.execute("SELECT * from us_cities where city iLIKE %(searchLike)s or zips iLIKE %(searchLike)s or state_id = %(search)s", {"searchLike": f"{search}%", "search": search})
   return cursor.fetchmany(5)
   close_postgres(connection, cursor)
 
