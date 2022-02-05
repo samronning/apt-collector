d12b9a5ca6e5882194f98fec066b1aa71d54dcec
Sam Ronning <sronning@umich.edu>
2022-02-04 21:12:09 -0500

    add 24hr cache for apartment listings

--- routes/apartments/apartments.py
+++ routes/apartments/apartments.py
@@ -1,19 +1,29 @@
 from flask import request
 from flask_restful import Resource
+import json
 
 #mylibs
 from .collector import get_page_data_by_area, get_all_data_by_area
 from .apartments_com_parser import listing_parser
 
 class AptList(Resource):
+    def __init__(self, r):
+        self.r = r
+
+
     def get(self):
         location = request.args.get('location')
         page = request.args.get('page')
+        #data is stale after 24 hours
+        timeout = 60 * 60 * 12
         if location is None:
             return "Must specify a location", 400
         if page is None:
+            if self.r.exists(location):
+                return json.loads(self.r.get(location)), 200
             raw = get_all_data_by_area(location)
             parsed = listing_parser(raw)
+            self.r.setex(location, timeout, json.dumps(parsed))
             return parsed, 200
         try:
             int(page)
@@ -22,8 +32,12 @@ class AptList(Resource):
         if int(page) < 0:
             return "Invalid Page", 400
         try:
+            rediskey = f"{location}-{page}"
+            if self.r.exists(rediskey):
+                return json.loads(self.r.get(rediskey))
             raw = get_page_data_by_area(location, page)
             parsed = listing_parser(raw)
+            self.r.setex(rediskey, timeout, json.dumps(parsed))
             return parsed, 200
         except Exception as inst:
             print("Exception!")
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from dotenv import load_dotenv
+from redisConnection import r
 import pandas as pd
 
 # mylibs
@@ -14,6 +15,6 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-api.add_resource(AptList, '/apartments/list')
+api.add_resource(AptList, '/apartments/list', resource_class_kwargs={"r": r})
 api.add_resource(CitySearch, '/cities')
 app.run()
